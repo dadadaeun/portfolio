ce8e0754c00aa08455af8426d7507978452dc9e7
dadadaeun <lasilver36@naver.com>
2021-11-04 15:45:03 +0900

    11/4-3

---
+++ js/common.js
@@ -0,0 +1,26 @@
+//menu toggle
+// 1. .header__button--menu 를 누르면 메뉴가 열린다. (단, 메뉴가 닫혀있을 때 == is-active 클래스가 없을때)
+// 2. .header__button--close 를 누르면 메뉴가 닫힌다. (단, 메뉴가 열려있을 때)
+function menuToggle() {
+    const openButton = document.querySelector('.header__button--menu');
+    const closeButton = document.querySelector('.header__button--close');
+    const menu = document.querySelector('.header__nav');
+    const activeClass = 'is-active';
+  
+    openButton.addEventListener('click', function() {
+      if(!menu.classList.contains(activeClass)) {
+        menu.classList.add(activeClass);
+      }
+    })
+  
+    closeButton.addEventListener('click',function(){
+      if(menu.classList.contains(activeClass)){
+        menu.classList.remove(activeClass);
+      }
+    })
+  }
+
+  
+document.addEventListener('DOMContentLoaded',function() {
+    menuToggle();
+  })
--- js/main.js
+++ js/main.js
@@ -15,28 +15,6 @@ const foodsSwiper = new Swiper(".swiper--foods", {
   },
 });
 
-//menu toggle
-// 1. .header__button--menu 를 누르면 메뉴가 열린다. (단, 메뉴가 닫혀있을 때 == is-active 클래스가 없을때)
-// 2. .header__button--close 를 누르면 메뉴가 닫힌다. (단, 메뉴가 열려있을 때)
-function menuToggle() {
-  const openButton = document.querySelector('.header__button--menu');
-  const closeButton = document.querySelector('.header__button--close');
-  const menu = document.querySelector('.header__nav');
-  const activeClass = 'is-active';
-
-  openButton.addEventListener('click', function() {
-    if(!menu.classList.contains(activeClass)) {
-      menu.classList.add(activeClass);
-    }
-  })
-
-  closeButton.addEventListener('click',function(){
-    if(menu.classList.contains(activeClass)){
-      menu.classList.remove(activeClass);
-    }
-  })
-}
-
 function visualAnimation() {
   const roulette = new slowingDownRotation({
     wrapperElement: document.querySelector('.visual'),
@@ -54,7 +32,6 @@ document.addEventListener('DOMContentLoaded',function() {
   bestSwiper.update();
   foodsSwiper.update();
 
-  menuToggle();
   //visualAnimation();
 })
 
